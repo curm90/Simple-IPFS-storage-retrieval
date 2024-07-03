df55ded0a492db9f65f51fc30fb778ffdfbd26d8
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-03 22:22:47 +0100

    successfully rendered images uploaded to IPFS

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,25 +2,37 @@
 
 import { useCallback, useState } from 'react';
 import { upload } from 'thirdweb/storage';
+import { MediaRenderer } from 'thirdweb/react';
 import { useDropzone } from 'react-dropzone';
 import client from '@/lib/utils/thirdweb';
 
 export default function Form() {
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    const uri = await upload({ client, files: acceptedFiles });
-    console.log({ acceptedFiles, uri });
-  }, []);
+  const [storedUris, setStoredUris] = useState<string[]>([]);
+
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      const uris = await upload({ client, files: acceptedFiles });
+      console.log({ acceptedFiles, uris });
+      setStoredUris(uris);
+    },
+    [upload],
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+    <div>
+      {storedUris.map((uri) => (
+        <MediaRenderer key={uri} client={client} src={uri} alt='Image' />
+      ))}
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
     </div>
   );
 }
