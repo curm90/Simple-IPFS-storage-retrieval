6a461a838ccb6167ad944e9f649e7229689ecd37
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-03 22:16:04 +0100

    fix env var

--- src/lib/utils/thirdweb.ts
+++ src/lib/utils/thirdweb.ts
@@ -1,9 +1,9 @@
 import { createThirdwebClient } from 'thirdweb';
 
-const THIRDWEB_CLIENT_ID = process.env.THIRDWEB_CLIENT_ID;
+const NEXT_PUBLIC_THIRDWEB_CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
 const client = createThirdwebClient({
-  clientId: THIRDWEB_CLIENT_ID as string,
+  clientId: NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
 });
 
 export default client;
